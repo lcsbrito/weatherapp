3224679b227a6de0cabf219f1c4a35865dac6078
Lucas Brito <lcsbrito.pro@gmail.com>
2024-04-30 11:43:23 +0200

    fix plugin error

--- app/index.tsx
+++ app/index.tsx
@@ -2,5 +2,8 @@ import { useState } from "react"
 import { XStack, YStack, Button, Input, Card } from "tamagui"
 
 export default function IndexScreen() {
-return null
+return(
+  <XStack fullscreen flex={1} backgroundColor={"$black025"}>
+    </XStack>
+)
 }
